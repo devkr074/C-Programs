1a709530d5e75a0b624ac57327e79b43d29eae11
Dev Kumar <devkr074@gmail.com>
2024-11-05 19:10:13 +0530

    AND operator in C

---
+++ Program_17.c
@@ -0,0 +1,18 @@
+// AND operator in C
+
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    float temp = 25;
+    bool sunny = true;
+    if (temp >= 0 && temp <= 30 && sunny)
+    {
+        printf("The weather is good!");
+    }
+    else
+    {
+        printf("The weather is bad!");
+    }
+    return 0;
+}
