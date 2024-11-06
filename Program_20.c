832323434626c4f1761ff945b94e4d057e6a2d0a
Dev Kumar <devkr074@gmail.com>
2024-11-06 17:54:02 +0530

    Function in C

---
+++ Program_20.c
@@ -0,0 +1,17 @@
+// Function in C
+
+#include <stdio.h>
+void birthday()
+{
+    printf("\nHappy Birthday to you\n");
+    printf("Happy Birthday to you\n");
+    printf("Happy Birthday dear ...\n");
+    printf("Happy Birthday to you\n");
+}
+int main()
+{
+    birthday();
+    birthday();
+    birthday();
+    return 0;
+}
