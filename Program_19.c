caec3fb5fda48846c336032b57d4143c061393c4
Dev Kumar <devkr074@gmail.com>
2024-11-06 17:53:26 +0530

    NOT operator in C

---
+++ Program_19.c
@@ -0,0 +1,17 @@
+// NOT operator in C
+
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    bool sunny = false;
+    if (!sunny)
+    {
+        printf("It's cloudy outside");
+    }
+    else
+    {
+        printf("It's sunny outside");
+    }
+    return 0;
+}
