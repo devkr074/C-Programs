252d019a2de89f15e6a23bc763844e5ff72b6b52
Dev Kumar <devkr074@gmail.com>
2025-10-26 22:21:04 +0530

    Print Text

--- Program_2.c
+++ Program_2.c
@@ -1,11 +1,7 @@
-// Escape Sequences in C
-
 #include <stdio.h>
-int main()
-{
-    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
-    printf("\"I Like Pizza\" - Abraham Lincoln\n");
-    printf("\'I Like Pizza\' - Abraham Lincoln\n");
-    printf("\\I Like Pizza\\ - Abraham Lincoln");
-    return 0;
+int main() {
+  printf("Hello World!");
+  printf("I am learning C.");
+  printf("And it is awesome!");
+  return 0;
 }
