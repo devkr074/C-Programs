d9676b54f2397a4e29562e52ec19376ef7af4412
Dev Kumar <devkr074@gmail.com>
2025-10-26 22:19:58 +0530

    Syntax

--- Program_1.c
+++ Program_1.c
@@ -1,16 +1,5 @@
-// Printing Messages and Comments in C
-
 #include <stdio.h>
-int main()
-{
-    // This is Single Line Comment
-    /*
-    This
-    is
-    Multi-Line
-    Comment
-    */
-    printf("I like pizza!\n");
-    printf("It's delicious\n");
-    return 0;
+int main() {
+  printf("Hello World!");
+  return 0;
 }
