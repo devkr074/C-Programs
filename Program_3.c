15a3c608939415745c0f30c788e8bebb1056936b
Dev Kumar <devkr074@gmail.com>
2025-10-26 22:21:52 +0530

    New Lines

--- Program_3.c
+++ Program_3.c
@@ -1,18 +1,9 @@
-// Variables in C
-
 #include <stdio.h>
-int main()
-{
-    int a;               // Declaration
-    a = 90;              // Initialization
-    int b = 70;          // Declaration + Initialization
-    int age = 21;        // Integer
-    float gpa = 2.05;    // Floating Point
-    char grade = 'C';    // Character
-    char name[] = "Bro"; // Array of Character (String)
-    printf("Hello %s\n", name);
-    printf("You are %d years old\n", age);
-    printf("Your average grade is %c\n", grade);
-    printf("Your GPA is %f", gpa);
-    return 0;
+int main() {
+  printf("Hello World!\n");
+  printf("I am learning C.");
+  printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+  printf("Hello World!\n\n");
+  printf("I am learning C.");
+  return 0;
 }
