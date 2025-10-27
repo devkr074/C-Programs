1ea1ac6081bde2f52f2ca75ac0079c59e47919e9
Dev Kumar <devkr074@gmail.com>
2025-10-27 19:44:17 +0530

    Change Values

--- Program_7.c
+++ Program_7.c
@@ -1,32 +1,16 @@
-// Arithmetic Operators in C
-
 #include <stdio.h>
 int main()
 {
-    // + (Addition)
-    // - (Subtraction)
-    // * (Multiplication)
-    // / (Division)
-    // % (Modulus)
-    // ++ (Increment)
-    // -- (Decrement)
+    int myNum = 15;
+    myNum = 10;
+    int myOtherNum = 23;
+    myNum = myOtherNum;
+    printf("%d", myNum);
+    myOtherNum = myNum;
+    printf("%d", myOtherNum);
     int x = 5;
-    int y = 2;
+    int y = 6;
     int sum = x + y;
-    int sub = x - y;
-    int mul = x * y;
-    int div1 = x / y;
-    float div2 = x / (float)y;
-    int rem = x % y;
-    x++;
-    y++;
-    printf("%d\n", sum);
-    printf("%d\n", sub);
-    printf("%d\n", mul);
-    printf("%d\n", div1);
-    printf("%f\n", div2);
-    printf("%d\n", rem);
-    printf("%d\n", x);
-    printf("%d", y);
+    printf("%d", sum);
     return 0;
 }
