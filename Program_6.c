d6b14817fb3ead6c7d5ab97926d087e0602ae585
Dev Kumar <devkr074@gmail.com>
2025-10-27 19:41:42 +0530

    Format Specifiers

--- Program_6.c
+++ Program_6.c
@@ -1,9 +1,15 @@
-// Constant in C
-
 #include <stdio.h>
 int main()
 {
-    const float PI = 3.14159;
-    printf("%f", PI);
+    int myNum = 15;
+    float myFloatNum = 5.99;
+    char myLetter = 'D';
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n", myLetter);
+    printf("My favorite number is: %d", myNum);
+    printf("My number is %d and my letter is %c", myNum, myLetter);
+    printf("My favorite number is: %d", 15);
+    printf("My favorite letter is: %c", 'D');
     return 0;
 }
