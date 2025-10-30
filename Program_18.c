731af01660abe8746ceb9da537a30f7c82cffff3
Dev Kumar <devkr074@gmail.com>
2025-10-30 22:21:56 +0530

    Extended Types

--- Program_18.c
+++ Program_18.c
@@ -1,18 +1,29 @@
-// OR operator in C
-
 #include <stdio.h>
-#include <stdbool.h>
 int main()
 {
-    float temp = 25;
-    bool sunny = true;
-    if (temp <= 0 || temp >= 30 || sunny)
-    {
-        printf("The weather is bad!");
-    }
-    else
-    {
-        printf("The weather is good!");
-    }
+    int normalInt = 1000;
+    double normalDouble = 3.14;
+    short int small = -100;
+    unsigned int count = 25;
+    long int big = 1234567890;
+    long long int veryBig = 9223372036854775807;
+    unsigned long long int huge = 18446744073709551615U;
+    long double precise = 3.141592653589793238L;
+    printf("Normal int: %d\n", normalInt);
+    printf("Normal double: %lf\n", normalDouble);
+    printf("Small: %hd\n", small);
+    printf("Count: %u\n", count);
+    printf("Big: %ld\n", big);
+    printf("Very Big: %lld\n", veryBig);
+    printf("Huge: %llu\n", huge);
+    printf("Precise: %Lf\n", precise);
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of double: %zu bytes\n", sizeof(double));
+    printf("Size of short int: %zu bytes\n", sizeof(short int));
+    printf("Size of unsigned int: %zu bytes\n", sizeof(unsigned int));
+    printf("Size of long int: %zu bytes\n", sizeof(long int));
+    printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+    printf("Size of unsigned long long int: %zu bytes\n", sizeof(unsigned long long int));
+    printf("Size of long double: %zu bytes\n", sizeof(long double));
     return 0;
 }
