0d9556d8f88a5ed22ed519e1b8e2e8e9e38f8959
Dev Kumar <devkr074@gmail.com>
2025-10-30 22:24:58 +0530

    Operators

--- Program_20.c
+++ Program_20.c
@@ -1,17 +1,13 @@
-// Function in C
-
 #include <stdio.h>
-void birthday()
-{
-    printf("\nHappy Birthday to you\n");
-    printf("Happy Birthday to you\n");
-    printf("Happy Birthday dear ...\n");
-    printf("Happy Birthday to you\n");
-}
 int main()
 {
-    birthday();
-    birthday();
-    birthday();
+    int myNum = 100 + 50;
+    int sum1 = 100 + 50;
+    int sum2 = sum1 + 250;
+    int sum3 = sum2 + sum2;
+    printf("My Number: %d\n", myNum);
+    printf("Sum1: %d\n", sum1);
+    printf("Sum2: %d\n", sum2);
+    printf("Sum3: %d\n", sum3);
     return 0;
 }
