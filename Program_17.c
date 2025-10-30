d824eeda2d32e26759ab00b9aee4012401fbb06f
Dev Kumar <devkr074@gmail.com>
2025-10-30 22:19:43 +0530

    Type Conversion

--- Program_17.c
+++ Program_17.c
@@ -1,18 +1,29 @@
-// AND operator in C
-
 #include <stdio.h>
-#include <stdbool.h>
 int main()
 {
-    float temp = 25;
-    bool sunny = true;
-    if (temp >= 0 && temp <= 30 && sunny)
-    {
-        printf("The weather is good!");
-    }
-    else
-    {
-        printf("The weather is bad!");
-    }
+    int x = 5;
+    int y = 2;
+    int sum = 5 / 2;
+    printf("%d", sum);
+    float myFloat = 9;
+    printf("%f", myFloat);
+    int myInt = 9.99;
+    printf("%d", myInt);
+    float sum = 5 / 2;
+    printf("%f", sum);
+    float sum = (float) 5 / 2;
+    printf("%f", sum);
+    int num1 = 5;
+    int num2 = 2;
+    float sum = (float) num1 / num2;
+    printf("%f", sum);
+    int num1 = 5;
+    int num2 = 2;
+    float sum = (float) num1 / num2;
+    printf("%.1f", sum);
+    int maxScore = 500;
+    int userScore = 423;
+    float percentage = (float) userScore / maxScore * 100.0;
+    printf("User's percentage is %.2f", percentage);
     return 0;
 }
