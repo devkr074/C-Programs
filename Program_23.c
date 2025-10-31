fa86157bed7327281a6619ee612c714e889c80c3
Dev Kumar <devkr074@gmail.com>
2025-10-31 22:07:27 +0530

    Comparison

--- Program_23.c
+++ Program_23.c
@@ -1,13 +1,14 @@
-// Ternary Operator in C
-
 #include <stdio.h>
-int findMax(int x, int y)
-{
-    return (x > y) ? x : y;
-}
 int main()
 {
-    int max = findMax(3, 5);
-    printf("%d", max);
+    int x = 5;
+    int y = 3;
+    printf("%d", x > y);
+    int age = 18;
+    printf("%d\n", age >= 18);
+    printf("%d\n", age < 18);
+    int passwordLength = 5;
+    printf("%d\n", passwordLength >= 8);
+    printf("%d\n", passwordLength < 8);
     return 0;
 }
