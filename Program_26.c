841693d63e1222b284156486fd53de0ef87bb9a2
Dev Kumar <devkr074@gmail.com>
2025-11-01 19:27:13 +0530

    Booleans

--- Program_26.c
+++ Program_26.c
@@ -1,11 +1,20 @@
-// for Loop in C
-
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
-    for (int i = 1; i <= 10; i++)
-    {
-        printf("%d\n", i);
-    }
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+    printf("%d", isProgrammingFun);
+    printf("%d", isFishTasty);
+    printf("%d", 10 > 9);
+    int x = 10;
+    int y = 9;
+    printf("%d", x > y);
+    printf("%d", 10 == 10);
+    printf("%d", 10 == 15);
+    printf("%d", 5 == 55);
+    bool isHamburgerTasty = true;
+    bool isPizzaTasty = true;
+    printf("%d", isHamburgerTasty == isPizzaTasty);
     return 0;
 }
