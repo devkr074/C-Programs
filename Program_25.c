1d8e3146260aa56c570c36de5647202c57860709
Dev Kumar <devkr074@gmail.com>
2025-11-01 19:24:44 +0530

    Precedence

--- Program_25.c
+++ Program_25.c
@@ -1,39 +1,13 @@
-// String Functions in C
-
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-    char str1[] = "Bro";
-    char str2[] = "Code";
-    int result;
-    strupr(str1);
-    printf("%s\n", str1);
-    strlwr(str1);
-    printf("%s\n", str1);
-    strcat(str1, str2);
-    printf("%s\n", str1);
-    strncat(str1, str2, 1);
-    printf("%s\n", str1);
-    strcpy(str1, str2);
-    printf("%s\n", str1);
-    strncpy(str1, str2, 1);
-    printf("%s", str1);
-    strset(str1, '#');
-    printf("%s", str1);
-    strnset(str1, 'x', 2);
-    printf("%s", str1);
-    strrev(str1);
-    printf("%s", str1);
-    result = strlen(str1);
-    printf("%d", result);
-    result = strcmp(str1, str2);
-    printf("%d", result);
-    result = strncmp(str1, str2, 1);
-    printf("%d", result);
-    result = strcmpi(str1, str2);
-    printf("%d", result);
-    result = strnicmp(str1, str2, 1);
-    printf("%d", result);
+    int result1 = 2 + 3 * 4;
+    int result2 = (2 + 3) * 4;
+    printf("%d\n", result1);
+    printf("%d\n", result2);
+    result1 = 10 - 2 + 5;
+    result2 = 10 - (2 + 5);
+    printf("%d\n", result1);
+    printf("%d\n", result2);
     return 0;
 }
