0356e07bdf704448b2949be9c38df5e8e073907b
Dev Kumar <devkr074@gmail.com>
2025-11-02 22:20:39 +0530

    Nested If

--- Program_32.c
+++ Program_32.c
@@ -1,12 +1,25 @@
-// Iterating Array in C using Loop
-
 #include <stdio.h>
 int main()
 {
-    int arr[] = {5, 7, 3, 2, 6, 8};
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
-    {
-        printf("%d\n", arr[i]);
+    int x = 15;
+    int y = 25;
+    if (x > 10) {
+        printf("x is greater than 10\n");
+        if (y > 20) {
+            printf("y is also greater than 20\n");
+        }
+    }
+    int age = 20;
+    bool isCitizen = true;
+    if (age >= 18) {
+        printf("Old enough to vote.\n");
+        if (isCitizen) {
+            printf("And you are a citizen, so you can vote!\n");
+        } else {
+            printf("But you must be a citizen to vote.\n");
+        }
+    } else {
+        printf("Not old enough to vote.\n");
     }
     return 0;
 }
