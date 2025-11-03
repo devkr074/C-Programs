ebc3c6821fafc2c6129d76d991e3e5d3957503b5
Dev Kumar <devkr074@gmail.com>
2025-11-03 20:30:07 +0530

    While Loop

--- Program_36.c
+++ Program_36.c
@@ -1,33 +1,16 @@
-// Sorting of an Array in C
-
 #include <stdio.h>
-void sortArray(int array[], int size)
-{
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-}
-void printArray(int array[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", array[i]);
-    }
-}
 int main()
 {
-    int arr[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    sortArray(arr, size);
-    printArray(arr, size);
+    int i = 0;
+    while (i < 5) {
+        printf("%d\n", i);
+        i++;
+    }
+    int countdown = 3;
+    while (countdown > 0) {
+        printf("%d\n", countdown);
+        countdown--;
+    }
+    printf("Happy New Year!!\n");
     return 0;
 }
