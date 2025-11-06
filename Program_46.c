319c016d728f3fbe9def2f5aa04c2c52ee0c59a4
Dev Kumar <devkr074@gmail.com>
2025-11-06 14:41:38 +0530

    Real-Life Example

--- Program_46.c
+++ Program_46.c
@@ -1,21 +1,32 @@
-// Pointers in C
-
 #include <stdio.h>
-void printAge(int *);
 int main()
 {
-    int age = 21;
-    int *pAge = &age;
-    printf("Address of Age : %p\n", &age);
-    printf("Value of pAge : %p\n", pAge);
-    printf("Size of Age : %d bytes\n", sizeof(age));
-    printf("Size of pAge : %d bytes\n", sizeof(pAge));
-    printf("Value of Age : %d\n", age);
-    printf("Value stored at %p address : %d\n", pAge, *pAge);
-    printAge(pAge);
+    int ages[] = {20, 22, 18, 35, 48, 26, 87, 70};
+    float avg, sum = 0;
+    int i;
+    int length = sizeof(ages) / sizeof(ages[0]);
+    for (i = 0; i < length; i++) {
+        sum += ages[i];
+    }
+    avg = sum / length;
+    printf("The average age is: %.2f", avg);
+    length = sizeof(ages) / sizeof(ages[0]);
+    int lowestAge = ages[0];
+    for (i = 0; i < length; i++) {
+        if (lowestAge > ages[i]) {
+            lowestAge = ages[i];
+        }
+    }
+    int myNumbers[] = {3, -1, 7, 0, 9};
+    length = sizeof(myNumbers) / sizeof(myNumbers[0]);
+    for (i = 0; i < length; i++) {
+        if (myNumbers[i] < 0) {
+            continue;
+        }
+        if (myNumbers[i] == 0) {
+            break;
+        }
+        printf("%d\n", myNumbers[i]);
+    }
     return 0;
-}
-void printAge(int *pAge)
-{
-    printf("Age : %d", *pAge);
 }
