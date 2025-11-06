3880bf3a21f996bd61215c9646497066fe6d6da3
Dev Kumar <devkr074@gmail.com>
2025-11-06 14:43:54 +0530

    Multidimensional Arrays

--- Program_47.c
+++ Program_47.c
@@ -1,11 +1,16 @@
-// Writing File in C
-
 #include <stdio.h>
 int main()
 {
-    FILE *pF = fopen("test.txt", "a");
-    fprintf(pF, "Spongebob Squarepants");
-    fprintf(pF, "\nPattrick Puff");
-    fclose(pF);
+    int matrix[2][3] = { {1, 4, 2}, {3, 6, 8} };
+    printf("%d", matrix[0][2]);
+    matrix[0][0] = 9;
+    printf("%d", matrix[0][0]);
+    int i, j;
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d\n", matrix[i][j]);
+        }
+    }
+    int example[2][4][3];
     return 0;
 }
