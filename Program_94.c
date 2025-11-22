cc3bd1fcb7f9b384a86f8d16fadc45499a468fad
Dev Kumar <devkr074@gmail.com>
2025-11-22 17:02:15 +0530

    Fixed-width Integers

---
+++ Program_94.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+int main() {
+    int8_t a = 100;
+    int16_t b = 30000;
+    int32_t c = 2000000;
+    int64_t d = 9000000000;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%lld\n", d);
+    uint8_t battery = 87;
+    printf("Battery level is %u out of 100\n", battery);
+    return 0;
+}
